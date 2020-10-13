e01cc5065a81d0577215946cd95194fafb403ff1
JefferyXiang <jeffrey.xiang@xiaofaai.com>
2020-10-13 17:14:01 +0800

    Button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,61 @@
 <template>
-    <div id="app">
-		app
-    </div>
+	<div id="app">
+		<div class="component-divide component-desc">
+			<a>
+				按钮：可以设置type为
+				<span>default</span>
+				<span>primary</span>
+				<span>dashed</span>
+				<span>text</span>
+				共四种类型
+			</a>
+		</div>
+		<div class="component-divide">
+			<v-button>default</v-button>
+			<v-button type="primary" class="mgl-20">primary</v-button>
+			<v-button type="dashed" class="mgl-20">dashed</v-button>
+			<v-button type="text" class="mgl-20">text</v-button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-    mounted () {},
-    methods: {}
-}
+	//Button组件
+	import VButton from './components/button';
+
+	export default {
+		components: {
+			VButton
+		},
+		mounted() {
+		},
+		methods: {
+		}
+	}
 </script>
 
 <style scoped lang="less">
+	.component-divide {
+		margin-top: 20px;
+		padding: 0 20px;
+	}
 
+	.component-desc a {
+		font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, \\5FAE\8F6F\96C5\9ED1, Arial, sans-serif;
+		font-size: 14px;
+		line-height: 1.5;
+		color: #515a6e;
+	}
+
+	.component-desc span {
+		background-color: #fff5f5;
+		color: #fa795e;
+		border-radius: 4px;
+		margin-left: 10px;
+		padding: 5px 10px;
+	}
+
+	.mgl-20 {
+		margin-left: 20px;
+	}
 </style>
--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,23 +1,50 @@
 <template>
-
+	<component :is="tagName" :class="classes" :disabled="itemDisabled" @click="handleClickLink" v-bind="tagProps">
+		<div class="vu-btn-slot" v-if="showSlot" ref="slot">
+			<slot></slot>
+		</div>
+	</component>
 </template>
 
 <script>
 	import {oneOf} from '../../utils/assist';
+	import mixinsForm from '../../mixins/form';
+	import mixinsLink from '../../mixins/link';
 
 	//样式的统一前缀
 	const prefixCls = 'vu-btn';
 
 	export default {
 		name: "button",
+		mixins: [mixinsForm, mixinsLink],
 		props: {
 			type: {
 				validator(value) {
-					return oneOf(value, ['default', 'primary', 'dashed', 'text', 'info', 'success', 'warning', 'error']);
+					return oneOf(value, ['default', 'primary', 'dashed', 'text']);
 				},
 				default: 'default'
 			},
 			isHrefPattern: {
+				type: Boolean,
+				default: false
+			},
+			long: {
+				type: Boolean,
+				default: false
+			},
+			shape: {
+				validator(value) {
+					return oneOf(value, ['circle', 'circle-outline']);
+				}
+			},
+			size: {
+				validator(value) {
+					return oneOf(value, ['small', 'large', 'default']);
+				},
+				default: 'default'
+			},
+			loading: Boolean,
+			ghost: {
 				type: Boolean,
 				default: false
 			}
@@ -26,10 +53,46 @@
 			tagName() {
 				return this.isHrefPattern ? 'a' : 'button';
 			},
+
+			classes() {
+				return [
+					`${prefixCls}`,
+					`${prefixCls}-${this.type}`,
+					{
+						[`${prefixCls}-long`]: this.long,
+						[`${prefixCls}-${this.shape}`]: !!this.shape,
+						[`${prefixCls}-${this.size}`]: this.size !== 'default',
+						[`${prefixCls}-loading`]: this.loading != null && this.loading,
+						[`${prefixCls}-ghost`]: this.ghost
+					}
+				];
+			},
+
+			tagProps() {
+				if (this.isHrefPattern) {
+					const {linkUrl, target} = this;
+					return {href: linkUrl, target};
+				} else {
+					const {htmlType} = this;
+					return {type: htmlType};
+				}
+			},
+
+			showSlot() {
+				console.log(this.$slots);
+				return !!this.$slots.default;
+			},
+		},
+		methods: {
+			handleClickLink(event) {
+				this.$emit('click', event);
+				const openInNewWindow = event.ctrlKey || event.metaKey;
+				this.handleCheckClick(event, openInNewWindow);
+			}
 		}
 	}
 </script>
 
-<style scoped>
-
+<style>
+	@import "../../styles/components/button.css";
 </style>
