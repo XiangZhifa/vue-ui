07b9a3a5579c84fdfad24ec71feb4157cd50df9a
JefferyXiang <jeffrey.xiang@xiaofaai.com>
2020-10-13 17:54:47 +0800

    Spin

---
+++ src/components/spin/spin.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+	import {oneOf} from '../../utils/assist';
+	import ScrollbarMixins from '../modal/mixins-scrollbar';
+
+	const prefixCls = 'ivu-spin';
+
+	export default {
+		name: "spin"
+	}
+</script>
+
+<style>
+
+</style>
